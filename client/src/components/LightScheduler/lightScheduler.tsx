4476de41850695c9bc7c8c146ebc43e446da6a80
wpwilson10 <wpwilson10@gmail.com>
2025-01-06 14:07:12 -0600

    Fix for #1 and extras
    Displays loading spinner instead of rendering early. Refactor for modularity. Success/fail banners clear after delay. Update terraform provider versions

--- client/src/components/LightScheduler/lightScheduler.tsx
+++ client/src/components/LightScheduler/lightScheduler.tsx
@@ -1,363 +1,360 @@
-"use client";
+'use client';
 
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from 'react';
 import {
-	Table,
-	Form,
-	Container,
-	Button,
-	InputGroup,
-	Row,
-	Col,
-	Alert,
-} from "react-bootstrap";
-import axios from "axios";
-import { handleAxiosError } from "../../utils/error";
-import LoadingSpinner from "../LoadingSpinner/spinner";
+  Table,
+  Form,
+  Container,
+  Button,
+  InputGroup,
+  Row,
+  Col,
+  Alert,
+} from 'react-bootstrap';
+import axios from 'axios';
+import { handleAxiosError } from '../../utils/error';
+import LoadingSpinner from '../LoadingSpinner/spinner';
 
 // The server URL for the contact form API.
 const lightScheduleURL: string =
-	process.env.API_DOMAIN_NAME! + process.env.LIGHT_SCHEDULE_API!;
+  process.env.API_DOMAIN_NAME! + process.env.LIGHT_SCHEDULE_API!;
 
 // Define an interface for the schedule entries
 interface ScheduleEntry {
-	id: number;
-	time: string;
-	warmBrightness: number;
-	coolBrightness: number;
+  id: number;
+  time: string;
+  warmBrightness: number;
+  coolBrightness: number;
 }
 
 // Define an interface for the server data structure
 interface ScheduleData {
-	mode: "dayNight" | "scheduled" | "demo";
-	schedule: ScheduleEntry[];
+  mode: 'dayNight' | 'scheduled' | 'demo';
+  schedule: ScheduleEntry[];
 }
 
 const isValidScheduleData = (data: any): data is ScheduleData => {
-	// Validate the mode
-	const isValidMode = ["dayNight", "scheduled", "demo"].includes(data?.mode);
-	// Validate the schedule array
-	const isValidSchedule =
-		Array.isArray(data?.schedule) &&
-		data.schedule.every(
-			(entry: any) =>
-				typeof entry.id === "number" &&
-				typeof entry.time === "string" &&
-				typeof entry.warmBrightness === "number" &&
-				entry.warmBrightness >= 0 &&
-				entry.warmBrightness <= 100 &&
-				typeof entry.coolBrightness === "number" &&
-				entry.coolBrightness >= 0 &&
-				entry.coolBrightness <= 100
-		);
+  // Validate the mode
+  const isValidMode = ['dayNight', 'scheduled', 'demo'].includes(data?.mode);
+  // Validate the schedule array
+  const isValidSchedule =
+    Array.isArray(data?.schedule) &&
+    data.schedule.every(
+      (entry: any) =>
+        typeof entry.id === 'number' &&
+        typeof entry.time === 'string' &&
+        typeof entry.warmBrightness === 'number' &&
+        entry.warmBrightness >= 0 &&
+        entry.warmBrightness <= 100 &&
+        typeof entry.coolBrightness === 'number' &&
+        entry.coolBrightness >= 0 &&
+        entry.coolBrightness <= 100
+    );
 
-	return isValidMode && isValidSchedule;
+  return isValidMode && isValidSchedule;
 };
 
-export const LightScheduler = () => {
-	const [data, setData] = useState<ScheduleData>({
-		mode: "scheduled",
-		schedule: [],
-	});
-	const [newTime, setNewTime] = useState("");
-	const [newWarmBrightness, setNewWarmBrightness] = useState("");
-	const [newCoolBrightness, setNewCoolBrightness] = useState("");
-	const [unsavedChanges, setUnsavedChanges] = useState(false);
-	// track form submission success or error
-	const [isSuccessfullySubmitted, setIsSuccessfullySubmitted] =
-		useState(false);
-	const [isSubmissionError, setIsSubmissionError] = useState(false);
+const ScheduleTable = ({ data, handleInputChange, handleRemoveRow }: any) => (
+  <Row className="justify-content-md-left">
+    <Table striped bordered hover className="mb-3">
+      <thead>
+        <tr>
+          <th>Time</th>
+          <th>Warm Brightness</th>
+          <th>Cool Brightness</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.schedule.map((entry: ScheduleEntry) => (
+          <tr key={entry.id}>
+            <td>{entry.time}</td>
+            <td>
+              <Form.Control
+                type="number"
+                value={entry.warmBrightness}
+                min="0"
+                max="100"
+                onChange={(e) =>
+                  handleInputChange(entry.id, 'warmBrightness', e.target.value)
+                }
+              />
+            </td>
+            <td>
+              <Form.Control
+                type="number"
+                value={entry.coolBrightness}
+                min="0"
+                max="100"
+                onChange={(e) =>
+                  handleInputChange(entry.id, 'coolBrightness', e.target.value)
+                }
+              />
+            </td>
+            <td>
+              <Button
+                variant="danger"
+                size="sm"
+                onClick={() => handleRemoveRow(entry.id)}
+              >
+                X
+              </Button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  </Row>
+);
 
-	// Load schedule from server
-	useEffect(() => {
-		const fetchSchedule = async () => {
-			try {
-				const response =
-					await axios.get<ScheduleData>(lightScheduleURL);
+const AddRowForm = ({
+  newTime,
+  newWarmBrightness,
+  newCoolBrightness,
+  setNewTime,
+  setNewWarmBrightness,
+  setNewCoolBrightness,
+  handleAddRow,
+}: any) => (
+  <Row className="justify-content-md-left">
+    <Col className="mb-3">
+      <h5>Add a New Row</h5>
+      <InputGroup className="mb-3">
+        <Form.Control
+          type="time"
+          value={newTime}
+          onChange={(e) => setNewTime(e.target.value)}
+        />
+        <Form.Control
+          type="number"
+          placeholder="Warm Brightness"
+          value={newWarmBrightness}
+          min="0"
+          max="100"
+          onChange={(e) => setNewWarmBrightness(e.target.value)}
+        />
+        <Form.Control
+          type="number"
+          placeholder="Cool Brightness"
+          value={newCoolBrightness}
+          min="0"
+          max="100"
+          onChange={(e) => setNewCoolBrightness(e.target.value)}
+        />
+        <Button variant="primary" onClick={handleAddRow}>
+          Add
+        </Button>
+      </InputGroup>
+    </Col>
+  </Row>
+);
 
-				// Validate response data
-				if (isValidScheduleData(response.data)) {
-					setData(response.data);
-				} else {
-					console.error(
-						"Invalid schedule data format",
-						response.data
-					);
-				}
-			} catch (error) {
-				handleAxiosError(error);
-			}
-		};
-		fetchSchedule();
-	}, []);
+const ModeSelector = ({ data, handleModeChange }: any) => (
+  <Row className="justify-content-start">
+    <Col xs="auto" md="auto" className="mb-3">
+      <Button
+        variant={data.mode === 'dayNight' ? 'primary' : 'outline-primary'}
+        onClick={() => handleModeChange('dayNight')}
+      >
+        Day/Night Cycle
+      </Button>
+    </Col>
+    <Col xs="auto" md="auto" className="mb-3">
+      <Button
+        variant={data.mode === 'scheduled' ? 'primary' : 'outline-primary'}
+        onClick={() => handleModeChange('scheduled')}
+      >
+        Scheduled
+      </Button>
+    </Col>
+    <Col xs="auto" md="auto" className="mb-3">
+      <Button
+        variant={data.mode === 'demo' ? 'primary' : 'outline-primary'}
+        onClick={() => handleModeChange('demo')}
+      >
+        Demo
+      </Button>
+    </Col>
+  </Row>
+);
 
-	// Save schedule to server
-	const saveSchedule = async () => {
-		try {
-			console.log(data);
-			await axios.post(lightScheduleURL, data);
-			setIsSubmissionError(false);
-			setIsSuccessfullySubmitted(false);
-			setUnsavedChanges(false);
-		} catch (error) {
-			handleAxiosError(error);
-			setIsSubmissionError(true);
-			setIsSuccessfullySubmitted(false);
-		}
-	};
+const LightScheduler = () => {
+  const [data, setData] = useState<ScheduleData>({
+    mode: 'scheduled',
+    schedule: [],
+  });
+  const [newTime, setNewTime] = useState('');
+  const [newWarmBrightness, setNewWarmBrightness] = useState('');
+  const [newCoolBrightness, setNewCoolBrightness] = useState('');
+  const [unsavedChanges, setUnsavedChanges] = useState(false);
+  const [isSuccessfullySubmitted, setIsSuccessfullySubmitted] = useState(false);
+  const [isSubmissionError, setIsSubmissionError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true); // Add loading state
 
-	const handleInputChange = (
-		id: number,
-		type: "warmBrightness" | "coolBrightness",
-		value: string
-	) => {
-		const updatedSchedule = data.schedule.map((entry) =>
-			entry.id === id
-				? {
-						...entry,
-						[type]: Math.min(100, Math.max(0, Number(value))), // Clamp values to 0-100
-					}
-				: entry
-		);
-		setData({ ...data, schedule: updatedSchedule });
-		setUnsavedChanges(true);
-	};
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      try {
+        const response = await axios.get<ScheduleData>(lightScheduleURL);
+        if (isValidScheduleData(response.data)) {
+          setData(response.data);
+        } else {
+          console.error('Invalid schedule data format', response.data);
+        }
+      } catch (error) {
+        handleAxiosError(error);
+      } finally {
+        setIsLoading(false); // Set loading to false after data is fetched
+      }
+    };
+    fetchSchedule();
+  }, []);
 
-	const handleRemoveRow = (id: number) => {
-		const updatedSchedule = data.schedule.filter(
-			(entry) => entry.id !== id
-		);
-		setData({ ...data, schedule: updatedSchedule });
-		setUnsavedChanges(true);
-	};
+  useEffect(() => {
+    if (isSuccessfullySubmitted) {
+      const timer = setTimeout(() => {
+        setIsSuccessfullySubmitted(false);
+      }, 5000); // Clear success banner after 5 seconds
+      return () => clearTimeout(timer);
+    }
+    return undefined;
+  }, [isSuccessfullySubmitted]);
 
-	const handleAddRow = () => {
-		if (newTime && newWarmBrightness !== "" && newCoolBrightness !== "") {
-			const newRow: ScheduleEntry = {
-				id: Date.now(),
-				time: newTime,
-				warmBrightness: Math.min(
-					100,
-					Math.max(0, Number(newWarmBrightness))
-				),
-				coolBrightness: Math.min(
-					100,
-					Math.max(0, Number(newCoolBrightness))
-				),
-			};
+  useEffect(() => {
+    if (isSubmissionError) {
+      const timer = setTimeout(() => {
+        setIsSubmissionError(false);
+      }, 5000); // Clear error banner after 5 seconds
+      return () => clearTimeout(timer);
+    }
+    return undefined;
+  }, [isSubmissionError]);
 
-			const updatedSchedule = [...data.schedule, newRow].sort((a, b) =>
-				a.time.localeCompare(b.time)
-			);
-			setData({ ...data, schedule: updatedSchedule });
-			setUnsavedChanges(true);
-			setNewTime("");
-			setNewWarmBrightness("");
-			setNewCoolBrightness("");
-		}
-	};
+  const saveSchedule = async () => {
+    try {
+      await axios.post(lightScheduleURL, data);
+      setIsSubmissionError(false);
+      setIsSuccessfullySubmitted(true);
+      setUnsavedChanges(false);
+    } catch (error) {
+      handleAxiosError(error);
+      setIsSubmissionError(true);
+      setIsSuccessfullySubmitted(false);
+    }
+  };
 
-	const handleModeChange = (newMode: "dayNight" | "scheduled" | "demo") => {
-		setData({ ...data, mode: newMode });
-		setUnsavedChanges(true);
-	};
+  const handleInputChange = (
+    id: number,
+    type: 'warmBrightness' | 'coolBrightness',
+    value: string
+  ) => {
+    const updatedSchedule = data.schedule.map((entry) =>
+      entry.id === id
+        ? {
+            ...entry,
+            [type]: Math.min(100, Math.max(0, Number(value))),
+          }
+        : entry
+    );
+    setData({ ...data, schedule: updatedSchedule });
+    setUnsavedChanges(true);
+  };
 
-	return (
-		<Suspense fallback={<LoadingSpinner />}>
-			<Container
-				id="light_scheduler"
-				className="content-container mb-3 py-3 px-3"
-			>
-				{/* Success or error message after submission */}
-				<Row className="justify-content-md-left">
-					<Col md={12} className="mb-3">
-						{/* Feedback for successful form submission */}
-						{isSuccessfullySubmitted && (
-							<Alert variant="success">
-								<p className="mb-0">
-									Success - Light Settings Updated
-								</p>
-							</Alert>
-						)}
-						{/* Feedback for unsuccessful form submission */}
-						{isSubmissionError && (
-							<Alert variant="danger">
-								<p className="mb-0">
-									Error occurred while sending updated
-									settings. Please try again.
-								</p>
-							</Alert>
-						)}
-					</Col>
-				</Row>
+  const handleRemoveRow = (id: number) => {
+    const updatedSchedule = data.schedule.filter((entry) => entry.id !== id);
+    setData({ ...data, schedule: updatedSchedule });
+    setUnsavedChanges(true);
+  };
 
-				<h5>Select Mode</h5>
-				<Row className="justify-content-start">
-					{" "}
-					{/* Align buttons to the left */}
-					<Col xs="auto" md="auto" className="mb-3">
-						{" "}
-						{/* Shrink the columns to fit content */}
-						<Button
-							variant={
-								data.mode === "dayNight"
-									? "primary"
-									: "outline-primary"
-							}
-							onClick={() => handleModeChange("dayNight")}
-						>
-							Day/Night Cycle
-						</Button>
-					</Col>
-					<Col xs="auto" md="auto" className="mb-3">
-						<Button
-							variant={
-								data.mode === "scheduled"
-									? "primary"
-									: "outline-primary"
-							}
-							onClick={() => handleModeChange("scheduled")}
-						>
-							Scheduled
-						</Button>
-					</Col>
-					<Col xs="auto" md="auto" className="mb-3">
-						<Button
-							variant={
-								data.mode === "demo"
-									? "primary"
-									: "outline-primary"
-							}
-							onClick={() => handleModeChange("demo")}
-						>
-							Demo
-						</Button>
-					</Col>
-				</Row>
+  const handleAddRow = () => {
+    if (newTime && newWarmBrightness !== '' && newCoolBrightness !== '') {
+      const newRow: ScheduleEntry = {
+        id: Date.now(),
+        time: newTime,
+        warmBrightness: Math.min(100, Math.max(0, Number(newWarmBrightness))),
+        coolBrightness: Math.min(100, Math.max(0, Number(newCoolBrightness))),
+      };
+      const updatedSchedule = [...data.schedule, newRow].sort((a, b) =>
+        a.time.localeCompare(b.time)
+      );
+      setData({ ...data, schedule: updatedSchedule });
+      setUnsavedChanges(true);
+      setNewTime('');
+      setNewWarmBrightness('');
+      setNewCoolBrightness('');
+    }
+  };
 
-				{data.mode !== "demo" && (
-					<Row>
-						<Row className="justify-content-md-left">
-							<Table striped bordered hover className="mb-3">
-								<thead>
-									<tr>
-										<th>Time</th>
-										<th>Warm Brightness</th>
-										<th>Cool Brightness</th>
-										<th>Actions</th>
-									</tr>
-								</thead>
-								<tbody>
-									{data.schedule.map((entry) => (
-										<tr key={entry.id}>
-											<td>{entry.time}</td>
-											<td>
-												<Form.Control
-													type="number"
-													value={entry.warmBrightness}
-													min="0"
-													max="100"
-													onChange={(e) =>
-														handleInputChange(
-															entry.id,
-															"warmBrightness",
-															e.target.value
-														)
-													}
-												/>
-											</td>
-											<td>
-												<Form.Control
-													type="number"
-													value={entry.coolBrightness}
-													min="0"
-													max="100"
-													onChange={(e) =>
-														handleInputChange(
-															entry.id,
-															"coolBrightness",
-															e.target.value
-														)
-													}
-												/>
-											</td>
-											<td>
-												<Button
-													variant="danger"
-													size="sm"
-													onClick={() =>
-														handleRemoveRow(
-															entry.id
-														)
-													}
-												>
-													X
-												</Button>
-											</td>
-										</tr>
-									))}
-								</tbody>
-							</Table>
-						</Row>
+  const handleModeChange = (newMode: 'dayNight' | 'scheduled' | 'demo') => {
+    setData({ ...data, mode: newMode });
+    setUnsavedChanges(true);
+  };
 
-						<Row className="justify-content-md-left">
-							<Col className="mb-3">
-								<h5>Add a New Row</h5>
-								<InputGroup className="mb-3">
-									<Form.Control
-										type="time"
-										value={newTime}
-										onChange={(e) =>
-											setNewTime(e.target.value)
-										}
-									/>
-									<Form.Control
-										type="number"
-										placeholder="Warm Brightness"
-										value={newWarmBrightness}
-										min="0"
-										max="100"
-										onChange={(e) =>
-											setNewWarmBrightness(e.target.value)
-										}
-									/>
-									<Form.Control
-										type="number"
-										placeholder="Cool Brightness"
-										value={newCoolBrightness}
-										min="0"
-										max="100"
-										onChange={(e) =>
-											setNewCoolBrightness(e.target.value)
-										}
-									/>
-									<Button
-										variant="primary"
-										onClick={handleAddRow}
-									>
-										Add
-									</Button>
-								</InputGroup>
-							</Col>
-						</Row>
-					</Row>
-				)}
+  return (
+    <Suspense fallback={<LoadingSpinner />}>
+      <Container
+        id="light_scheduler"
+        className="content-container mb-3 py-3 px-3"
+      >
+        {isLoading ? (
+          <LoadingSpinner /> // Display spinner while loading
+        ) : (
+          <>
+            <Row className="justify-content-md-left">
+              <Col md={12} className="mb-3">
+                {isSuccessfullySubmitted && (
+                  <Alert variant="success">
+                    <p className="mb-0">Success - Light Settings Updated</p>
+                  </Alert>
+                )}
+                {isSubmissionError && (
+                  <Alert variant="danger">
+                    <p className="mb-0">
+                      Error occurred while sending updated settings. Please try
+                      again.
+                    </p>
+                  </Alert>
+                )}
+              </Col>
+            </Row>
 
-				{/* Submit button aligned to the left*/}
-				<Row className="justify-content-md-left">
-					<Col md={12} className="mb-3 d-flex justify-content-end">
-						<Button
-							variant="success"
-							onClick={() => saveSchedule()} // Proper wrapping for the async function
-							disabled={!unsavedChanges}
-						>
-							Save Changes
-						</Button>
-					</Col>
-				</Row>
-			</Container>
-		</Suspense>
-	);
+            <h5>Select Mode</h5>
+            <ModeSelector data={data} handleModeChange={handleModeChange} />
+
+            {data.mode !== 'demo' && (
+              <>
+                <ScheduleTable
+                  data={data}
+                  handleInputChange={handleInputChange}
+                  handleRemoveRow={handleRemoveRow}
+                />
+                <AddRowForm
+                  newTime={newTime}
+                  newWarmBrightness={newWarmBrightness}
+                  newCoolBrightness={newCoolBrightness}
+                  setNewTime={setNewTime}
+                  setNewWarmBrightness={setNewWarmBrightness}
+                  setNewCoolBrightness={setNewCoolBrightness}
+                  handleAddRow={handleAddRow}
+                />
+              </>
+            )}
+
+            <Row className="justify-content-md-left">
+              <Col md={12} className="mb-3 d-flex justify-content-end">
+                <Button
+                  variant="success"
+                  onClick={() => saveSchedule()}
+                  disabled={!unsavedChanges}
+                >
+                  Save Changes
+                </Button>
+              </Col>
+            </Row>
+          </>
+        )}
+      </Container>
+    </Suspense>
+  );
 };
 
 export default LightScheduler;
--- terraform/.terraform.lock.hcl
+++ terraform/.terraform.lock.hcl
@@ -2,84 +2,24 @@
 # Manual edits may be lost in future updates.
 
 provider "registry.terraform.io/hashicorp/aws" {
-  version     = "5.79.0"
-  constraints = ">= 4.36.0, >= 4.40.0, >= 5.12.0, >= 5.37.0, >= 5.70.0, >= 5.79.0, 5.79.0"
+  version     = "5.82.2"
+  constraints = ">= 4.36.0, >= 4.40.0, >= 5.12.0, >= 5.37.0, >= 5.70.0, 5.82.2"
   hashes = [
-    "h1:jWOG+JhHLmfd20Zj/kve97+1e2eBwU8lETOVYnR08Ew=",
-    "zh:008b605b7b6dcde4eb86759f54a36db731f94649f780738c6918ef3826eb064f",
-    "zh:08c1dd9a2b4b0d45356fc2124ac292aa549aab9053e33e240dd322700082c132",
-    "zh:0fa102804fc3903a598b631a791c40fd285162738c2939e92980078bb5c58bf8",
-    "zh:217f19d86f51e89ef479aa6b08ad3205c9ffe1d60422bbe10b373232658c56c2",
-    "zh:5ebf88b696c15dcd5e9a8ec3e7c58ecc6c1939bc75b72710bb8454c8a59dabce",
-    "zh:77da434b802735cdac2c5f4cd28b18a8221de4ec443019d4e17beab0aa064b09",
-    "zh:7a3cdb0f3dbc0cc6e50e3e719c48513d73a23acc154c2a6fa3a501cc86f02831",
-    "zh:878dc6b3c5d3068f439f1617051f3d2da9b899741fcc66d98a46a8bacf4fb320",
-    "zh:940ead03bdf6401ed921b71a6e14edbb613fd1f0eefc41bf555bc3d499cc604e",
+    "h1:ce6Dw2y4PpuqAPtnQ0dO270dRTmwEARqnfffrE1VYJ8=",
+    "zh:0262fc96012fb7e173e1b7beadd46dfc25b1dc7eaef95b90e936fc454724f1c8",
+    "zh:397413613d27f4f54d16efcbf4f0a43c059bd8d827fe34287522ae182a992f9b",
+    "zh:436c0c5d56e1da4f0a4c13129e12a0b519d12ab116aed52029b183f9806866f3",
+    "zh:4d942d173a2553d8d532a333a0482a090f4e82a2238acf135578f163b6e68470",
+    "zh:624aebc549bfbce06cc2ecfd8631932eb874ac7c10eb8466ce5b9a2fbdfdc724",
     "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
-    "zh:a3b496cf3a537a1b74e1433c7a4c5b19d51ed7c094c640dd240e474886b8c1de",
-    "zh:baac56fafa9d4aa6102554c9c6ca340512c18a31bf0c9899f7e0e823bb18a32a",
-    "zh:e1560da6a1cb052f719199f20f0238d4fab606f54f3ddad1b09ad390ab5c4a46",
-    "zh:e3fb0ad64e1812f4560396c2660e729c760ecb593f3fd1b0d76101d6923e8230",
-    "zh:fdc5159df807242e884b108746ac4ddf9725b7ce9f425785d4af465845de280e",
-  ]
-}
-
-provider "registry.terraform.io/hashicorp/external" {
-  version     = "2.3.4"
-  constraints = ">= 1.0.0"
-  hashes = [
-    "h1:fjJwsIgh+BJEy8FsSt6HD0rKgA9iDCC+Rkv7IGNdNxc=",
-    "zh:037fd82cd86227359bc010672cd174235e2d337601d4686f526d0f53c87447cb",
-    "zh:0ea1db63d6173d01f2fa8eb8989f0809a55135a0d8d424b08ba5dabad73095fa",
-    "zh:17a4d0a306566f2e45778fbac48744b6fd9c958aaa359e79f144c6358cb93af0",
-    "zh:298e5408ab17fd2e90d2cd6d406c6d02344fe610de5b7dae943a58b958e76691",
-    "zh:38ecfd29ee0785fd93164812dcbe0664ebbe5417473f3b2658087ca5a0286ecb",
-    "zh:59f6a6f31acf66f4ea3667a555a70eba5d406c6e6d93c2c641b81d63261eeace",
-    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
-    "zh:ad0279dfd09d713db0c18469f585e58d04748ca72d9ada83883492e0dd13bd58",
-    "zh:c69f66fd21f5e2c8ecf7ca68d9091c40f19ad913aef21e3ce23836e91b8cbb5f",
-    "zh:d4a56f8c48aa86fc8e0c233d56850f5783f322d6336f3bf1916e293246b6b5d4",
-    "zh:f2b394ebd4af33f343835517e80fc876f79361f4688220833bc3c77655dd2202",
-    "zh:f31982f29f12834e5d21e010856eddd19d59cd8f449adf470655bfd19354377e",
-  ]
-}
-
-provider "registry.terraform.io/hashicorp/local" {
-  version     = "2.5.2"
-  constraints = ">= 1.0.0"
-  hashes = [
-    "h1:6NIiHWMbE9bFZaUiqC+OokdWSbW7g3+yQYnO4yvgtuY=",
-    "zh:136299545178ce281c56f36965bf91c35407c11897f7082b3b983d86cb79b511",
-    "zh:3b4486858aa9cb8163378722b642c57c529b6c64bfbfc9461d940a84cd66ebea",
-    "zh:4855ee628ead847741aa4f4fc9bed50cfdbf197f2912775dd9fe7bc43fa077c0",
-    "zh:4b8cd2583d1edcac4011caafe8afb7a95e8110a607a1d5fb87d921178074a69b",
-    "zh:52084ddaff8c8cd3f9e7bcb7ce4dc1eab00602912c96da43c29b4762dc376038",
-    "zh:71562d330d3f92d79b2952ffdda0dad167e952e46200c767dd30c6af8d7c0ed3",
-    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
-    "zh:805f81ade06ff68fa8b908d31892eaed5c180ae031c77ad35f82cb7a74b97cf4",
-    "zh:8b6b3ebeaaa8e38dd04e56996abe80db9be6f4c1df75ac3cccc77642899bd464",
-    "zh:ad07750576b99248037b897de71113cc19b1a8d0bc235eb99173cc83d0de3b1b",
-    "zh:b9f1c3bfadb74068f5c205292badb0661e17ac05eb23bfe8bd809691e4583d0e",
-    "zh:cc4cbcd67414fefb111c1bf7ab0bc4beb8c0b553d01719ad17de9a047adff4d1",
-  ]
-}
-
-provider "registry.terraform.io/hashicorp/null" {
-  version     = "3.2.3"
-  constraints = ">= 2.0.0"
-  hashes = [
-    "h1:zxoDtu918XPWJ/Y6s4aFrZydn6SfqkRc5Ax1ZLnC6Ew=",
-    "zh:22d062e5278d872fe7aed834f5577ba0a5afe34a3bdac2b81f828d8d3e6706d2",
-    "zh:23dead00493ad863729495dc212fd6c29b8293e707b055ce5ba21ee453ce552d",
-    "zh:28299accf21763ca1ca144d8f660688d7c2ad0b105b7202554ca60b02a3856d3",
-    "zh:55c9e8a9ac25a7652df8c51a8a9a422bd67d784061b1de2dc9fe6c3cb4e77f2f",
-    "zh:756586535d11698a216291c06b9ed8a5cc6a4ec43eee1ee09ecd5c6a9e297ac1",
-    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
-    "zh:9d5eea62fdb587eeb96a8c4d782459f4e6b73baeece4d04b4a40e44faaee9301",
-    "zh:a6355f596a3fb8fc85c2fb054ab14e722991533f87f928e7169a486462c74670",
-    "zh:b5a65a789cff4ada58a5baffc76cb9767dc26ec6b45c00d2ec8b1b027f6db4ed",
-    "zh:db5ab669cf11d0e9f81dc380a6fdfcac437aea3d69109c7aef1a5426639d2d65",
-    "zh:de655d251c470197bcbb5ac45d289595295acb8f829f6c781d4a75c8c8b7c7dd",
-    "zh:f5c68199f2e6076bce92a12230434782bf768103a427e9bb9abee99b116af7b5",
+    "zh:9e632dee2dfdf01b371cca7854b1ec63ceefa75790e619b0642b34d5514c6733",
+    "zh:a07567acb115b60a3df8f6048d12735b9b3bcf85ec92a62f77852e13d5a3c096",
+    "zh:ab7002df1a1be6432ac0eb1b9f6f0dd3db90973cd5b1b0b33d2dae54553dfbd7",
+    "zh:bc1ff65e2016b018b3e84db7249b2cd0433cb5c81dc81f9f6158f2197d6b9fde",
+    "zh:bcad84b1d767f87af6e1ba3dc97fdb8f2ad5de9224f192f1412b09aba798c0a8",
+    "zh:cf917dceaa0f9d55d9ff181b5dcc4d1e10af21b6671811b315ae2a6eda866a2a",
+    "zh:d8e90ecfb3216f3cc13ccde5a16da64307abb6e22453aed2ac3067bbf689313b",
+    "zh:d9054e0e40705df729682ad34c20db8695d57f182c65963abd151c6aba1ab0d3",
+    "zh:ecf3a4f3c57eb7e89f71b8559e2a71e4cdf94eea0118ec4f2cb37e4f4d71a069",
   ]
 }
